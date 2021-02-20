3aa88a72749366456a978623dd0683d1bac900b2
abdelaziz sekouti <abdelaziz-sekouti@hotmail.com>
2021-02-20 20:25:02 +0100

    glyphiconl2end

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
 var h = document.querySelectorAll('.panel-title');
 var ancre = document.querySelectorAll('.ancre');
 
-h.forEach(function(ht) {
+ancre.forEach(function(ht) {
 	ht.addEventListener('click', deploier);
 });
 
 function deploier(e) {
-	if (e.target.classList.contains('ancre')) {
+	if (e.target.firstElementChild.classList.contains('ancre')) {
 		ancre.forEach(function(anc) {
 			anc.firstElementChild.classList.add('glyphicon-chevron-right');
-			// anc.firstElementChild.setAttribute('title', 'click to see infos');
 			anc.firstElementChild.classList.remove('glyphicon-chevron-down');
 		});
 	}
 	else {
-		// ancre.forEach(function(anc) {
-		// 	anc.firstElementChild.setAttribute('title', '');
-		// });
 		anc.target.firstElementChild.classList.toggle('glyphicon-chevron-down');
 	}
 }
