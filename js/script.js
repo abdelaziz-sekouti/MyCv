13970448c79e8d84f50c3eb608d6b416a2a8542d
abdelaziz sekouti <abdelaziz-sekouti@hotmail.com>
2021-02-20 20:44:15 +0100

    glyphicons span2

--- js/script.js
+++ js/script.js
@@ -6,15 +6,13 @@ ancre.forEach(function(ht) {
 });
 
 function deploier(e) {
-	if (e.target.classList.contains('panel-title')) {
-		if (e.target.firstElementChild.firstElementChild.classList.contains('glyphicon-chevron-right')) {
-			ancre.forEach(function(anc) {
-				anc.firstElementChild.classList.add('glyphicon-chevron-right');
-				anc.firstElementChild.classList.remove('glyphicon-chevron-down');
-			});
-		}
-		else {
-			e.target.firstElementChild.firstElementChild.classList.toggle('glyphicon-chevron-down');
-		}
+	if (e.target.firstElementChild.firstElementChild.classList.contains('glyphicon-chevron-right')) {
+		ancre.forEach(function(anc) {
+			anc.firstElementChild.classList.add('glyphicon-chevron-right');
+			anc.firstElementChild.classList.remove('glyphicon-chevron-down');
+		});
+	}
+	else {
+		e.target.firstElementChild.firstElementChild.classList.toggle('glyphicon-chevron-down');
 	}
 }
