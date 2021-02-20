6a16bdc3326635016e53e3d8a95cac48f4ce6aa2
abdelaziz sekouti <abdelaziz-sekouti@hotmail.com>
2021-02-20 19:52:02 +0100

    titl

--- js/script.js
+++ js/script.js
@@ -9,14 +9,12 @@ function deploier(e) {
 	if (e.target.classList.contains('ancre')) {
 		ancre.forEach(function(anc) {
 			anc.firstElementChild.classList.add('glyphicon-chevron-right');
-			anc.setAttribute('title', 'click to see infos');
+			anc.firstElementChild.setAttribute('title', 'click to see infos');
 			anc.firstElementChild.classList.remove('glyphicon-chevron-down');
 		});
 	}
-	else {
-		ancre.forEach(function(anc) {
-			anc.setAttribute('title', '');
-		});
-		e.target.firstElementChild.classList.toggle('glyphicon-chevron-down');
-	}
+	ancre.forEach(function(anc) {
+		anc.firstElementChild.setAttribute('title', '');
+	});
+	e.target.firstElementChild.classList.toggle('glyphicon-chevron-down');
 }
