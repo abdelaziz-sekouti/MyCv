7483ce35d363db084bf5bc20e7c7e91f464b7672
abdelaziz sekouti <abdelaziz-sekouti@hotmail.com>
2021-02-20 19:24:59 +0100

    title

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 var h = document.querySelectorAll('.panel-title');
 var ancre = document.querySelectorAll('.ancre');
 
-h.forEach(function (ht) {
-    ht.addEventListener('click', deploier);
+h.forEach(function(ht) {
+	ht.addEventListener('click', deploier);
 });
 
 function deploier(e) {
-
-
-    if (e.target.classList.contains('ancre')) {
-        ancre.forEach(function (anc) {
-            anc.firstElementChild.classList.add('glyphicon-chevron-right');
-            anc.firstElementChild.classList.remove('glyphicon-chevron-down');
-        });
-        e.target.firstElementChild.classList.toggle('glyphicon-chevron-down');
-    }
+	if (e.target.classList.contains('ancre')) {
+		ancre.forEach(function(anc) {
+			anc.firstElementChild.classList.add('glyphicon-chevron-right');
+			anc.firstElementChild.classList.setAttribute('title', 'click to see infos');
+			anc.firstElementChild.classList.remove('glyphicon-chevron-down');
+		});
+		e.target.firstElementChild.classList.toggle('glyphicon-chevron-down');
+		anc.firstElementChild.classList.setAttribute('title', '');
+	}
 }
-
