9f838ed0fc0e4d043928d76b18f7cd3b53258a04
abdelaziz sekouti <abdelaziz-sekouti@hotmail.com>
2021-02-22 14:28:44 +0100

    chevron right or down pb resolved

--- js/script.js
+++ js/script.js
@@ -10,8 +10,18 @@ ancre.forEach(function(anc) {
 });
 //function to call when  ancre is clicked
 function deploier(e) {
+	console.log(e.target.parentElement.parentElement.nextElementSibling.className);
 	ancre.forEach(function(anc) {
+		//reset chevron right class to all span ancre's child
 		anc.firstElementChild.className = 'glyphicon glyphicon-chevron-right';
+		// check if collapsed div is collapse or not
+		if (e.target.parentElement.parentElement.nextElementSibling.classList.contains('in')) {
+			//div collapse then affect chevron down to span
+			e.target.firstElementChild.className = 'glyphicon glyphicon-chevron-right';
+		}
+		else {
+			//div not collapsed then affect chevron right to span
+			e.target.firstElementChild.className = 'glyphicon glyphicon-chevron-down';
+		}
 	});
-	e.target.firstElementChild.classList.toggle('glyphicon-chevron-down');
 }
